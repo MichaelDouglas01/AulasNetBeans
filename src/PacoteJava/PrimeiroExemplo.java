ce0bc1d9644e273121e0273a388b17ea930a14d9
MichaelDouglas01 <69487247+MichaelDouglas01@users.noreply.github.com>
2020-08-30 15:50:13 -0300

    Iniciando projeto java

---
+++ src/PacoteJava/PrimeiroExemplo.java
@@ -0,0 +1,37 @@
+
+package PacoteJava;
+    
+public class PrimeiroExemplo {
+    
+    static double valor1, valor2, resultado=0;
+    
+    public static void main(String args[]){
+        
+        //SegundoExemplo.escrever();
+        
+        int num1, num2, resp=0;
+        final double pi = 3.14161835;
+        
+        num1 = 5;
+        num2 = 15;
+        resp = num1 + num2;
+        
+        System.out.println("A Soma é: "+resp);  
+        
+        valor1=50;
+        valor2=50;
+        
+        resultado = valor1 * valor2 * pi;
+      
+        System.out.println("O valor é: "+resultado);
+        
+        
+       double numero = 3.258;
+       
+       int numerodois = (int) numero;
+        
+        System.out.println("Imprime = "+numerodois);
+    }
+    
+      
+}
